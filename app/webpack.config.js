4f686e9ba1250ef3a94bd62fb3c5b3ae029ec6a0
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-11-09 08:25:30 +0530

    setup: React App

---
+++ app/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  name: "React App",
+  mode: "development",
+  target: "web",
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+
+  resolve: {
+    roots: [path.resolve(__dirname, "app")],
+    modules: ["node_modules", path.resolve(__dirname, "app")]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)(s|sx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: [
+              ["module-resolver"],
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  regenerator: true,
+                },
+              ],
+            ]
+          }
+        }
+      }, {
+        test: /\.html$/i,
+        loader: "html-loader",
+      }
+    ]
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "app/public"),
+      serveIndex: true,
+      watch: true,
+    },
+    compress: true,
+    port: 9000,
+    watchFiles: [
+      "./app/**/*.js",
+      "./app/**/*.ts",
+      "./app/**/*.jsx",
+      "./app/**/*.tsx"
+    ]
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, "index.html")
+    })
+  ]
+}
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -12,37 +12,7 @@ module.exports = {
   resolve: {
     roots: [path.resolve(__dirname, "server")],
     modules: ["node_modules", "./server"],
-    extensions: [".js", ".json"],
-    // alias: {
-    //   assert: "assert",
-    //   buffer: "buffer",
-    //   console: "console-browserify",
-    //   constants: "constants-browserify",
-    //   crypto: "crypto-browserify",
-    //   domain: "domain-browser",
-    //   events: "events",
-    //   http: "stream-http",
-    //   https: "https-browserify",
-    //   os: "os-browserify/browser",
-    //   path: "path-browserify",
-    //   punycode: "punycode",
-    //   process: "process/browser",
-    //   querystring: "querystring-es3",
-    //   stream: "stream-browserify",
-    //   _stream_duplex: "readable-stream/duplex",
-    //   _stream_passthrough: "readable-stream/passthrough",
-    //   _stream_readable: "readable-stream/readable",
-    //   _stream_transform: "readable-stream/transform",
-    //   _stream_writable: "readable-stream/writable",
-    //   string_decoder: "string_decoder",
-    //   sys: "util",
-    //   timers: "timers-browserify",
-    //   tty: "tty-browserify",
-    //   url: "url",
-    //   util: "util",
-    //   vm: "vm-browserify",
-    //   zlib: "browserify-zlib"
-    // }
+    extensions: [".js", ".json"]
   },
 
   module: {
